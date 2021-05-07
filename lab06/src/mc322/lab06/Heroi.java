faf79bdcc567a92a87969598ba54abe0d174b42f
Wallace Lima <wallacegsantoslima@gmail.com>
2021-05-07 11:57:40 -0300

    Cria Heroi.java

---
+++ lab06/src/mc322/lab06/Heroi.java
@@ -0,0 +1,16 @@
+package mc322.lab06;
+
+
+public class Heroi extends Componente
+{
+    /**
+     * O herói deve ser uma componente do tipo agente inteligente, ou seja,
+     * deve ter conhecimento sobre si e sobre a caverna.
+     * Portanto deve ser capaz de se comunicar com a caverna e solicitar
+     * informações. 
+     * Além disso, o herói é quem recebe os comandos no teclado e realiza
+     * as movimentações do jogo*/
+    private boolean flecha;
+    private Caverna caverna;
+
+}
