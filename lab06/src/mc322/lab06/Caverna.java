d84ee56ae344974d802e703f56bbc3a41e0fdf17
Wallace Lima <wallacegsantoslima@gmail.com>
2021-05-07 11:38:19 -0300

    Cria Caverna.java

---
+++ lab06/src/mc322/lab06/Caverna.java
@@ -0,0 +1,20 @@
+package mc322.lab06;
+
+public class Caverna
+{
+    private Sala salas[][];
+
+    /**
+     * @param entrada: arquivo CSV com informações de como montar a caverna
+     */
+    Caverna(String entrada)
+    {
+        //
+    }
+
+    // recebe uma solicitação e executa a movimentação do herói se for possivel.
+    public void SolicitaMovimento(char comando)
+    {
+        //
+    }
+}
