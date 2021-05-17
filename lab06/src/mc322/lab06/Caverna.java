a431da61c3bcebddbb9f5c511909cf17e251db70
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-05-17 00:56:01 -0300

    Update Caverna.java

--- lab06/src/mc322/lab06/Caverna.java
+++ lab06/src/mc322/lab06/Caverna.java
@@ -26,10 +26,29 @@ public class Caverna
     public Sala getSala(int iSala, int jSala) {
     	return this.salas[iSala][jSala];
     }
+    
+    // Falta analisar o caso em que ele tenta sair da caverna sem o Ouro.
+    public int analisarFimDeMovimento(int iComponente, int jComponente, Componente Heroi) {
+    	if (this.salas[iComponente][jComponente].getComponentePrimario() instanceof Buraco) {
+    		return 0;
+    	}
+    	
+    	else if (this.salas[iComponente][jComponente].getComponentePrimario() instanceof Wumpus) {
+    		Heroi.iniciarLuta();
+    	}
+    	
+    	else if (this.salas[iComponente][jComponente].getComponentePrimario() instanceof Ouro) {
+    		Heroi.pegarOuro();
+    	}
+    }
 
     // recebe uma solicitação e executa a movimentação do herói se for possivel.
-    public void SolicitaMovimento(char comando)
+    public void solicitaMovimento(char comando)
     {
         //
+    }
+    
+    public void imprimir() {
+    	//
     }
 }
