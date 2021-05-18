508f7b9478997987afc1c84c1b6f6a498e9f4490
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-05-18 19:56:12 -0300

    Update Caverna.java

--- lab06/src/mc322/lab06/Caverna.java
+++ lab06/src/mc322/lab06/Caverna.java
@@ -19,13 +19,18 @@ public class Caverna
     }
     
     
-    public void setComponente(int iComponente, int jComponente, Componente componente) {
-    	this.salas[iComponente][jComponente].setComponentePrimario(componente);
+    public boolean setComponente(int iComponente, int jComponente, Componente componente) {
+    	if (this.salas[iComponente][jComponente].setComponentePrimario(componente)) {
+    		return true;
+    	}
+    	return false;
     }
+    
     
     public Sala getSala(int iSala, int jSala) {
     	return this.salas[iSala][jSala];
     }
+    
     
     // Falta analisar o caso em que ele tenta sair da caverna sem o Ouro.
     public int analisarFimDeMovimento(int iComponente, int jComponente, Componente Heroi) {
@@ -34,21 +39,35 @@ public class Caverna
     	}
     	
     	else if (this.salas[iComponente][jComponente].getComponentePrimario() instanceof Wumpus) {
-    		Heroi.iniciarLuta();
+    		return Heroi.iniciarLuta();
     	}
     	
     	else if (this.salas[iComponente][jComponente].getComponentePrimario() instanceof Ouro) {
     		Heroi.pegarOuro();
+    		return 1;
     	}
+    	
+    	return 1;
     }
 
+    
     // recebe uma solicitação e executa a movimentação do herói se for possivel.
     public void solicitaMovimento(char comando)
     {
         //
     }
     
+    
     public void imprimir() {
-    	//
+    	for (int i = 0; i < 4; i++) {
+    		System.out.print(i+1);
+    		for (int j = 0; j < 4; j++) {
+    			System.out.print(" " + this.salas[i][j].representacao());
+    		}
+    		System.out.println();
+    	}
+    	for (int i = 0; i < 4; i++) {
+    		System.out.print(" " + i+1);
+    	}
     }
 }
