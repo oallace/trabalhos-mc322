99344383982385d81bf79f9ef05bb34b8f63fb4c
Wallace Lima <wallacegsantoslima@gmail.com>
2021-05-19 14:54:22 -0300

    Atualiza Brisa

--- lab06/src/mc322/lab06/Buraco.java
+++ lab06/src/mc322/lab06/Buraco.java
@@ -1,8 +1,21 @@
 package mc322.lab06;
 
 public class Buraco extends Componente {
-	
-	Buraco(Caverna caverna, int iComponente, int jComponente, String Name){
-    	super(caverna, iComponente, jComponente, Name);
-    }
+
+	Buraco(Caverna caverna, int iComponente, int jComponente){
+    	super(caverna, iComponente, jComponente);
+		prioridade = 0;
+	}
+
+
+	public void removerComponente()
+	{
+		super.removerComponente();
+		// remover brisas
+	}
+
+	public char representacao()
+	{
+		return 'B';
+	}
 }
