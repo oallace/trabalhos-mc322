efc4dcdb948e0e11192c7d708b3c7946b73ae40f
Wallace Lima <wallacegsantoslima@gmail.com>
2021-05-19 14:24:22 -0300

    Atualiza Componenetes

--- lab06/src/mc322/lab06/Componente.java
+++ lab06/src/mc322/lab06/Componente.java
@@ -3,80 +3,32 @@ package mc322.lab06;
 public abstract class Componente
 {
 	protected Caverna caverna;
-	protected Sala sala;
 	protected int posicao[] = new int[2];
-	protected int score;
-	protected boolean flecha;
-	protected String Name;
+	protected int prioridade;	
 	
-	
-	Componente(Caverna caverna, int iComponente, int jComponente, String Name) {
+	Componente(Caverna caverna, int iComponente, int jComponente) {
 		this.caverna = caverna;
-		this.sala = null;
 		this.posicao[0] = iComponente;
 		this.posicao[1] = jComponente;
-		this.score = 0;
-		this.flecha = true;
-		this.Name = Name;
 	}
-	
-	
-	public Caverna getCaverna() {
-		return this.caverna;
-	}
-	
-	public Sala getSala() {
-		return this.sala;
-	}
-	
-	public void setSala(Sala sala) {
-		this.sala = sala;
-	}
-	
 	
 	public int[] getPosicao() {
 		return this.posicao;
 	}
-	
 	
 	public void setPosicao(int iPosicaoFinal, int jPosicaoFinal) {
 		this.posicao[0] = iPosicaoFinal;
 		this.posicao[1] = jPosicaoFinal;
 	}
 	
-	
-	// Determinado Componente Primário inicializa seus componentes secundários nas salas.
-	public void setComponentesSecundarios() {
-		Componente componente;
-		int iAtual = this.posicao[0];
-		int jAtual = this.posicao[1];
-		
-		if ((iAtual-1) >= 0) {
-			componente = (this instanceof Wumpus) ? new Fedor(this.caverna, iAtual-1, jAtual, "Fedor") : new Brisa(this.caverna, iAtual-1, jAtual, "Brisa");
-			this.caverna.getSala(iAtual-1, jAtual).setComponenteSecundario(componente);
-		}
-		if ((iAtual+1) < 4) {
-			componente = (this instanceof Wumpus) ? new Fedor(this.caverna, iAtual-1, jAtual, "Fedor") : new Brisa(this.caverna, iAtual-1, jAtual, "Brisa");
-			this.caverna.getSala(iAtual+1, jAtual).setComponenteSecundario(componente);
-		}
-		if ((jAtual-1) >= 0) {
-			componente = (this instanceof Wumpus) ? new Fedor(this.caverna, iAtual-1, jAtual, "Fedor") : new Brisa(this.caverna, iAtual-1, jAtual, "Brisa");
-			this.caverna.getSala(iAtual, jAtual-1).setComponenteSecundario(componente);
-		}
-		if ((jAtual+1) < 4) {
-			componente = (this instanceof Wumpus) ? new Fedor(this.caverna, iAtual-1, jAtual, "Fedor") : new Brisa(this.caverna, iAtual-1, jAtual, "Brisa");
-			this.caverna.getSala(iAtual, jAtual+1).setComponenteSecundario(componente);
-		}
-		
-		
+	public int getPrioridade()
+	{
+		return this.prioridade;
 	}
-	
-	
-	public abstract void iniciarLuta();
-	
-	public abstract void pegarOuro();
-	
-	public abstract void destruirComponente();
+		
+	public abstract void removerComponente();
 	
 	public abstract char representacao();
+
+	public abstract void apresentar();
 }
