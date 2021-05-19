91aec973e284a02d3ad660dd43d7522ca0c105d2
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-05-18 21:12:06 -0300

    Create Fedor.java

---
+++ lab06/src/mc322/lab06/Fedor.java
@@ -0,0 +1,8 @@
+package mc322.lab06;
+
+public class Fedor extends Componente{
+	
+	Fedor(Caverna caverna, int iComponente, int jComponente, String Name){
+    	super(caverna, iComponente, jComponente, Name);
+    }
+}
