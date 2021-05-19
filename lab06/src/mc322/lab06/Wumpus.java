2883438bf9cdb47aef5aa954b7df4c34cd715214
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-05-18 21:10:30 -0300

    Create Wumpus.java

---
+++ lab06/src/mc322/lab06/Wumpus.java
@@ -0,0 +1,8 @@
+package mc322.lab06;
+
+public class Wumpus extends Componente{
+	
+	Wumpus(Caverna caverna, int iComponente, int jComponente, String Name){
+    	super(caverna, iComponente, jComponente, Name);
+    }
+}
