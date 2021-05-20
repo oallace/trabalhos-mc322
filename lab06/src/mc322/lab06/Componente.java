2b45edcb613c4544883625df62e95fcd56df8be4
Wallace Lima <wallacegsantoslima@gmail.com>
2021-05-19 22:10:00 -0300

    Atualização

--- lab06/src/mc322/lab06/Componente.java
+++ lab06/src/mc322/lab06/Componente.java
@@ -5,6 +5,12 @@ public abstract class Componente {
 	protected int posicao[] = new int[2];
 	protected int prioridade;
 
+	/**
+	 * @param prioridade: indica a prioridade do componente sendo elas 0: Wumpus,
+	 *                    Buraco e Ouro; 1: Herói; 2: Fedor; 3: Brisa. Uma sala não
+	 *                    suporta dois componentes de mesma prioridade.
+	 */
+
 	Componente(Caverna caverna, int iComponente, int jComponente) {
 		this.caverna = caverna;
 		setPosicao(iComponente, jComponente);
@@ -23,14 +29,15 @@ public abstract class Componente {
 	public int getPrioridade() {
 		return this.prioridade;
 	}
-	
-	// Acomoda o componente na sua sala correspondente
+
+	// Acomoda o componente na sua sala correspondente.
+	// Retorna true se o componente pôde ser alocado.
 	public boolean acomodarCompontente() {
 		return caverna.setComponente(posicao[0], posicao[1], this);
 	}
 
-	public void removerComponente()
-	{
+	// Remove o componente da sala
+	public void removerComponente() {
 		caverna.removerComponente(this.posicao[0], this.posicao[1], this.prioridade);
 	}
 
