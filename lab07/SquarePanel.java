c0eb7cb61fc1585e3fe69ceb750af23215feaa94
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-06-01 19:53:10 -0300

    Add files via upload

---
+++ lab07/SquarePanel.java
@@ -0,0 +1,13 @@
+package mc322.lab07;
+
+import javax.swing.JPanel;
+
+public class SquarePanel extends JPanel{
+
+	private static final long serialVersionUID = 4934802567644563818L;
+	
+	SquarePanel(){
+		super();
+		
+	}
+}
