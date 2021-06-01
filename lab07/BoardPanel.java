ca34d263bd7adf0e10d69fb214db65799170e1e6
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-06-01 19:51:35 -0300

    Update BoardPanel.java

--- lab07/BoardPanel.java
+++ lab07/BoardPanel.java
@@ -1,6 +1,7 @@
 package mc322.lab07;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel{
@@ -12,7 +13,27 @@ public class BoardPanel extends JPanel{
 		
 		setSize(650, 650);
 		setLocation(20, 65);
-		//setLayout(new gridLayout());
-		setBackground(Color.BLUE);
+		setLayout(new GridLayout(8, 8));
+		setBackground(Color.LIGHT_GRAY);
+		int aux = 0;
+		
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (aux == 0) {
+					SquarePanel squarePanel = new SquarePanel();
+					squarePanel.setBackground(new Color(244, 241, 214)); //244, 241, 214
+					this.add(squarePanel);
+					aux = 1;
+				}
+				else {
+					SquarePanel squarePanel = new SquarePanel();
+					squarePanel.setBackground(new Color(128, 165, 91));  //128, 165, 91
+					this.add(squarePanel);
+					aux = 0;
+				}
+			}
+			
+			aux = (aux == 1) ? 0:1;
+		}
 	}
 }
