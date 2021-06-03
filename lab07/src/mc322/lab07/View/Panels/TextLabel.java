d408e30ac03659bf0a1c43ca03e59dbafc6af3aa
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-06-03 15:00:45 -0300

    Add files via upload

---
+++ lab07/src/mc322/lab07/View/Panels/TextLabel.java
@@ -0,0 +1,21 @@
+package mc322.lab07;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class TextLabel extends JLabel{
+
+	private static final long serialVersionUID = 3198200384504031621L;
+	
+	TextLabel(String text, int iPos, int jPos, int width, int height, int font, int rFont, int gFont, int bFont){
+		super(text);
+		
+		setFont(new Font("Arial", 0, font));
+		setForeground(new Color(rFont, gFont, bFont));
+		setLocation(iPos, jPos);
+		setSize(width, height);
+
+	}
+}
