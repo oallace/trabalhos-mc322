92ac11f28ac8c9c8cc6e574c8d35dbd377d921cb
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-06-03 08:55:37 -0300

    Update NameText.java

--- lab07/NameText.java
+++ lab07/NameText.java
@@ -10,19 +10,19 @@ public class NameText extends JPanel{
 
 	private static final long serialVersionUID = 8846260413030371668L;
 	
-	NameText(String text, int iPos, int jPos){
+	NameText(String text, int iPos, int jPos, int width, int height, int font){
 		super();
 		
-		setSize(200, 20);
+		setSize(width, height);
 		setLocation(iPos, jPos);
-		setBackground(Color.BLUE);
+		setBackground(Color.DARK_GRAY);
 		setLayout(null);
 		
 		JLabel jLabelText = new JLabel(text);
-		jLabelText.setFont(new Font("Arial", 0, 20));
+		jLabelText.setFont(new Font("Arial", 0, font));
 		jLabelText.setForeground(Color.white);
 		jLabelText.setLocation(0, 0);
-		jLabelText.setSize(200, 20);
+		jLabelText.setSize(width, height);
 
 		
 		this.add(jLabelText);
