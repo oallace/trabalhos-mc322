5dea08ef0c175c16ea40127c83e53736bf81acdf
Wallace Lima <wallacegsantoslima@gmail.com>
2021-06-17 14:30:41 -0300

    Correções de Warnings

--- lab07/src/mc322/lab07/view/panels/ImageLabel.java
+++ lab07/src/mc322/lab07/view/panels/ImageLabel.java
@@ -2,9 +2,11 @@ package mc322.lab07.view.panels;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import java.io.Serial;
 
 public class ImageLabel extends JLabel{
 
+	@Serial
 	private static final long serialVersionUID = -1623857887826457114L;
 	
 	public ImageLabel(String imagePath, int iPos, int jPos, int width, int height){
